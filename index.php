1fe2dd0f21b421d72bb2327c575dc4ccaf32f17b
Akeops <lalanne.andoni1@gmail.com>
2023-09-25 23:40:40 +0200

    Ajouut de fichier

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITwatchdog</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href=#>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="about">
+            //ici un commentaire
+            <div class="container">
+
+            </div>
+
+        </section>
+
+    </main>
+    
+</body>
+</html>
